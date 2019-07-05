c863fd05fb62cba3a40749cb3d5630f8f9ccc662
aylaylay <fortblox@gmail.com>
2019-07-05 15:33:33 +0100

    Window size and title bullshit

--- DonutGame/DonutGame/Program.cs
+++ DonutGame/DonutGame/Program.cs
@@ -1,10 +1,25 @@
 ﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
 
 namespace DonutGame
 {
     public class Game : GameWindow
     {
+        public Game() :
+            base(1280, 720, GraphicsMode.Default, "Plow Team", GameWindowFlags.FixedWindow)
+        {
+        }
 
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.ClearColor(Color4.Yellow);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            SwapBuffers();
+        }
     }
 
     class Program
