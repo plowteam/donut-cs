8977992b0772a688ac4e52ca8d390e56b4e6fdda
aylaylay <fortblox@gmail.com>
2019-07-05 15:19:21 +0100

    OpenTK bullshit

---
+++ DonutGame/DonutGame/Program.cs
@@ -0,0 +1,20 @@
+﻿using OpenTK;
+
+namespace DonutGame
+{
+    public class Game : GameWindow
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
