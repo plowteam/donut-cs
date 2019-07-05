84b55d2b0eeb4edad1cfada72c2a1d832785a13d
aylaylay <fortblox@gmail.com>
2019-07-05 15:44:24 +0100

    Copy triangle example bullshit just so we can see something rendering

--- DonutGame/DonutGame/Program.cs
+++ DonutGame/DonutGame/Program.cs
@@ -1,4 +1,5 @@
-﻿using OpenTK;
+﻿using System;
+using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 
@@ -6,17 +7,121 @@ namespace DonutGame
 {
     public class Game : GameWindow
     {
+        // A simple vertex shader possible. Just passes through the position vector.
+        const string VertexShaderSource = @"
+            #version 330
+            layout(location = 0) in vec4 position;
+            void main(void)
+            {
+                gl_Position = position;
+            }
+        ";
+
+        // A simple fragment shader. Just a constant red color.
+        const string FragmentShaderSource = @"
+            #version 330
+            out vec4 outputColor;
+            void main(void)
+            {
+                outputColor = vec4(1.0, 0.0, 0.0, 1.0);
+            }
+        ";
+
+        // Points of a triangle in normalized device coordinates.
+        readonly float[] Points = new float[] {
+            // X, Y, Z, W
+            -0.5f, 0.0f, 0.0f, 1.0f,
+            0.5f, 0.0f, 0.0f, 1.0f,
+            0.0f, 0.5f, 0.0f, 1.0f };
+
+        int VertexShader;
+        int FragmentShader;
+        int ShaderProgram;
+        int VertexBufferObject;
+        int VertexArrayObject;
+
         public Game() :
-            base(1280, 720, GraphicsMode.Default, "Plow Team", GameWindowFlags.FixedWindow)
+            base(1280, 720, GraphicsMode.Default, "Plow Team", GameWindowFlags.Default)
         {
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            // Load the source of the vertex shader and compile it.
+            VertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(VertexShader, VertexShaderSource);
+            GL.CompileShader(VertexShader);
+
+            // Load the source of the fragment shader and compile it.
+            FragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(FragmentShader, FragmentShaderSource);
+            GL.CompileShader(FragmentShader);
+
+            // Create the shader program, attach the vertex and fragment shaders and link the program.
+            ShaderProgram = GL.CreateProgram();
+            GL.AttachShader(ShaderProgram, VertexShader);
+            GL.AttachShader(ShaderProgram, FragmentShader);
+            GL.LinkProgram(ShaderProgram);
+
+            // Create the vertex buffer object (VBO) for the vertex data.
+            VertexBufferObject = GL.GenBuffer();
+            // Bind the VBO and copy the vertex data into it.
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, Points.Length * sizeof(float), Points, BufferUsageHint.StaticDraw);
+
+            // Retrive the position location from the program.
+            var positionLocation = GL.GetAttribLocation(ShaderProgram, "position");
+
+            // Create the vertex array object (VAO) for the program.
+            VertexArrayObject = GL.GenVertexArray();
+            // Bind the VAO and setup the position attribute.
+            GL.BindVertexArray(VertexArrayObject);
+            GL.VertexAttribPointer(positionLocation, 4, VertexAttribPointerType.Float, false, 0, 0);
+            GL.EnableVertexAttribArray(positionLocation);
+
+            GL.ClearColor(Color4.Yellow);
+        }
+
+        protected override void OnUnload(EventArgs e)
+        {
+            base.OnUnload(e);
+
+            // Unbind all the resources by binding the targets to 0/null.
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+            GL.UseProgram(0);
+
+            // Delete all the resources.
+            GL.DeleteBuffer(VertexBufferObject);
+            GL.DeleteVertexArray(VertexArrayObject);
+            GL.DeleteProgram(ShaderProgram);
+            GL.DeleteShader(FragmentShader);
+            GL.DeleteShader(VertexShader);
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, Width, Height);
         }
 
         protected override void OnRenderFrame(FrameEventArgs e)
         {
             base.OnRenderFrame(e);
 
-            GL.ClearColor(Color4.Yellow);
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            // Bind the VBO
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            // Bind the VAO
+            GL.BindVertexArray(VertexArrayObject);
+            // Use/Bind the program
+            GL.UseProgram(ShaderProgram);
+            // This draws the triangle.
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
 
             SwapBuffers();
         }
